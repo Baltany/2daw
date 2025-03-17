b7e3126df5a9300b804247b718b5995ec1ddb554
Balbino Moyano López <bmoylop0903@g.educaand.es>
2025-03-17 14:36:19 +0100

    picando codigo

--- DWES/relacion1.php
+++ DWES/relacion1.php
@@ -31,3 +31,38 @@ if(esBisiesto($year)){
     echo "El año $year no es bisiesto. ";
 }
 ?>
+
+<!-- Mostrar en pantalla una tabla de 10 por 10 con
+  los números impares a partir de 
+uno generado al azar. Se debe ver en 
+el navegador los bordes de la tabla. -->
+<?php
+
+    function genNumeroAleatorio($num1,$num2) {
+        return rand($num1,$num2);
+    }
+
+    $primerNumero = genNumeroAleatorio(1,100);
+    if($primerNumero % 2 == 0){
+        $primerNumero++;
+    }
+
+    echo "<table border='1' style='border-collapse: collapse;'>";
+
+    $numero = $primerNumero;
+
+    for($i=0;$i<10;$i++){
+        echo "<tr>";
+        for($j=0;$j<10;$j++){
+            echo "<td style='border: 1px solid black;'>$numero</td>";
+            $numero+=2;
+        }
+        echo "</tr>";
+    }
+
+    echo "</table>";
+
+?>
+
+
+
