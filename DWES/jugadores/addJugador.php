82580d2616570223205df144440eec4380c43ff7
Baltany <bmoylop0903@g.educaand.es>
2025-04-23 09:56:18 +0200

    Conexion a la bbdd

--- DWES/jugadores/addJugador.php
+++ DWES/jugadores/addJugador.php
@@ -3,14 +3,24 @@
 require_once 'conexion.php';
 require_once 'funciones.php';
 if(!isset($_POST['enviar'])){
-
+    //leemos las posiciones que hay
+    $posiciones = getPosiciones();
 ?>
 <form action="" method="POST">
     Nombre: <input type="text" name="nombre"><br>
     DNI: <input type="text" name="dni"><br>
     Dorsal: <input type="text" name="dorsal"><br>
-    <!-- TENGO QUE RECOGER LOS REGISTROS DEL SET QUE HAY EN LA BBDD POR/DEF/MED/DEL-->
-    Posicion: <select type="text" name="posicion"><br>
+    <!-- Recorremos las posiciones que hay en la bbdd-->
+    Posicion: <select type="text" name="posicion[]" multiple>
+            <?php
+            foreach($posiciones as $posicion){
+            ?>
+            <option value="<?php echo $posicion;?>"></option>    
+            <?php
+            }
+            ?>
+            </select>
+    <br>
     Equipo: <input type="text" name="equipo"><br>
     Goles: <input type="text" name="goles"><br>
     <input type="submit" name="enviar" value="Enviar">
--- DWES/jugadores/funciones.php
+++ DWES/jugadores/funciones.php
@@ -5,13 +5,31 @@ require_once "conexion.php";
 // Insert into jugadores values(1,"lamine","12345678x","19","DELANTERO","FCBARCELONA","5");
 function addJugador($nombre,$dni,$dorsal,$posicion,$equipo,$goles){
     global $connection;
-
-    $stmt = $connection -> prepare("INSER INTO jugadores(id,nombre, dni, dorsal, posicion, equipo, goles) VALUES(?.?,?,?,?,?,?)");
+    
+    $stmt = $connection -> prepare("INSER INTO jugadores(nombre, dni, dorsal, posicion, equipo, goles) VALUES(?.?,?,?,?,?,?)");
     if($stmt === false){
         die("Error en la preparacion ". $connection->error);
     }
     // En este caso como estoy usando todo en la base de datos como string usamos 's' si usara algun entero sería 'i'
-    $stmt -> bind_param("ssssss",$id,$nombre,$dni,$dorsal,$posicion,$equipo,$goles);
+    $stmt -> bind_param("ssssss",$nombre,$dni,$dorsal,$posicion,$equipo,$goles);
+    
+    // Comprobamos que se ha ejecutado correctamente
+    if($stmt-> execute()){
+        echo "Jugador insertado correctamente,Filas afectadas: " . $stmt->affected_rows;
+    }else{
+        echo "Error de insercion ". $stmt->error;
+    }
+    
+    // Una vez terminamos la sentencia y ejecutada cerramos la conexion
+    $stmt->close();
+}
+
+function getPosicion(){
+    global $connection;
+    $result = $connection->query("SELECT DISTINCT posicion FROM jugadores");
+    $posiciones = [];
+    
+
 }
 
 ?>
