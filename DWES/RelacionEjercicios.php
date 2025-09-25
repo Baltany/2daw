c4086140d5cb16e6f9c61f9d68508195f0437816
root <root@balbino.balbino>
2025-09-25 10:57:38 +0200

    Rehaciendo los ejercicios

--- DWES/RelacionEjercicios.php
+++ DWES/RelacionEjercicios.php
@@ -46,4 +46,44 @@ for ($i=0;$i<10;$i++){
 echo "</table>";
 ?>
 
+<?php
+function esMayor($a,$b,$c){
+    if($a>$b && $a>$c){
+        return $a;
+    }else if($b>$c && $b>$a){
+        return $b;
+    }else{
+        return $c;
+    }
+
+$a = 100;
+$b = 5000;
+$c = 1;
+
+echo "El número mayor es: " . esMayor($a, $b, $c);
+}
+?>
+
+
+<?php
+echo "<table border='1' style='border'>";
+$i = 0;
+while($i<5){
+    echo "<tr>";
+    $j = 0;
+    while($j<7){
+        echo "<td>" . ($i*7 + $j + 1). "</td>";
+        $j++;
+    }
+    echo "</tr>";
+    $i++;
+
+}
+echo "</table>";
+
+?>
+
+<?php
+
+?>
 
