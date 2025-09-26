795f4f827a9f081b3c5645bab9e208e134603f74
root <root@balbino.balbino>
2025-09-26 10:39:53 +0200

    Rehaciendo los ejercicios

---
+++ DWES/Moyano_Lopez_Balbino/RelacionEjercicios.php
@@ -0,0 +1,152 @@
+<?php
+function esBisiesto($year){
+    if($year % 400 == 0){
+        return true;
+    } else if($year % 100 ==0){
+        return true;
+
+    }else if($year % 4 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+$year = 2025;
+if(esBisiesto($year)){
+    echo "El año $year es bisiesto";
+}
+else{
+    echo "El año $year no es bisiesto";
+}
+?>
+
+<?php
+function generarNumAleatorio($num1,$num2){
+    return rand($num1,$num2);
+}
+
+$primerNumero = genNumeroAleatorio(1,100);
+if($primerNumero % 2 ==0){
+    $primerNumero++;
+}
+
+echo "<table border='1'>";
+
+$numero = $primerNumero;
+
+for ($i=0;$i<10;$i++){
+    echo "<tr>";
+    for($j=0;$j<10;$j++){
+        echo "<td style='border: 1px solid black;> $numero </td>";
+        $numero+=2;
+    }
+    echo "</tr>";
+}
+echo "</table>";
+?>
+
+<?php
+function esMayor($a,$b,$c){
+    if($a>$b && $a>$c){
+        return $a;
+    }else if($b>$c && $b>$a){
+        return $b;
+    }else{
+        return $c;
+    }
+
+$a = 100;
+$b = 5000;
+$c = 1;
+
+echo "El número mayor es: " . esMayor($a, $b, $c);
+}
+?>
+
+
+<?php
+echo "<table border='1' style='border'>";
+$i = 0;
+while($i<5){
+    echo "<tr>";
+    $j = 0;
+    while($j<7){
+        echo "<td>" . ($i*7 + $j + 1). "</td>";
+        $j++;
+    }
+    echo "</tr>";
+    $i++;
+
+}
+echo "</table>";
+
+?>
+
+<?php
+$suma = 0;
+for($i=1; $i<=100; $i++){
+    $suma += $i;
+}
+echo "La suma de los numeros enteros es: $suma";
+
+$suma = 0;
+$i = 1;
+while ($i <=100){
+    $suma += $i;
+    $i++;
+}
+echo "La suma de los numeros enteros es: $suma";
+
+$suma = 0;
+$i = 1;
+do{
+    $suma+=$i;
+    $i++;
+}while($i <= 100);
+echo $suma;
+?>
+
+<?php
+$suma = 0;
+for($i=1; $i<=100; $i++){
+    $suma += $i * $i;
+}
+echo "La suma de los cuadrados de los 100 primeros numeros enteros es: $suma";
+?>
+
+
+<?php
+$suma = 0;
+for($i=2; $i<=200; $i+=2){
+    $suma += $i;
+}
+echo "La suma de los 100 primeros numeros pares es: $suma";
+
+?>
+
+
+<?php
+$a = 10;
+$b = 20;
+$c = 30;
+
+if($a < $b){
+    $aux = $a;
+    $a = $b;
+    $b = $aux;
+    }
+if($a < $c){
+    $aux = $a;
+    $a = $c;
+    $c = $aux;
+}
+if($b < $c){
+    $aux = $b;
+    $b = $c;
+    $c = $aux;
+}
+echo "El orden de los numeros es: $a, $b, $c";
+
+
+?>
