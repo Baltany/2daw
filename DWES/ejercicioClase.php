16d15b44e50eaddd94a534eeddb8b76c133bc60d
root <root@balbino.balbino>
2025-09-30 13:33:43 +0200

    Ejercicio clase

--- DWES/ejercicioClase.php
+++ DWES/ejercicioClase.php
@@ -17,14 +17,62 @@ foreach($_SERVER as $key => $value){
 <?php
 $matriz = array(
     "Marketing" => array(
-        "nombre" => "pepe",
+        "nombre" => "Pepe",
         "apellidos" => "López",
         "Salario" => 1500,
         "Edad" => 35
     ),
+    "Dirección" => array(
+        "nombre" => "Juan",
+        "apellidos" => "López",
+        "Salario" => 1000,
+        "Edad" => 46
+    ),
     "Contabilidad" => array(
-        "nombre" => "Juan"
+        "nombre" => "Jose",
+        "apellidos" => "López",
+        "Salario" => 1500,
+        "Edad" => 35
+    ),
+    "Ventas" => array(
+        "nombre" => "Antonio",
+        "apellidos" => "López",
+        "Salario" => 1900,
+        "Edad" => 50
+    ),
+    "Información" => array(
+        "nombre" => "Pedro",
+        "apellidos" => "López",
+        "Salario" => 1800,
+        "Edad" => 38
     )
 );
+echo "<br>";
+
+echo "<table border='1'>";
+echo "<tr>";
+echo "<th> </th>";
+foreach ($matriz["Ventas"] as $clave=>$valor){
+    echo "<th>";
+    echo $clave;
+    echo "</th>";
+}
+echo "</tr>";
+
+
+foreach($matriz as $clave=>$fila){
+    echo "<tr>";
+    echo "<th>". $clave ."</th>";
+    foreach($fila as $valor){
+        echo "<td>";
+        echo $valor;
+        echo "</td>";
+        
+
+    }
+    echo "</tr>";
+}
+
+echo "</table>"
 
 ?>
